53f17dba3e94828738ec95103a8c76c18fadefaf
Salvador Acién Gómez <salvadoraciengomez@gmail.com>
2021-05-05 19:12:42 +0200

    Using Inquirer menu #2

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -4,20 +4,50 @@ require('colors');
 const preguntas=[
     {
         type: 'list',
-        name: 'option',
+        name: 'opcion',
         message: '¿Qué deseas hacer?',
-        choices: ['opt1','opt2','opt3']
+        choices: [
+            {
+                vaule: '1',
+                name: '1. Crear tarea'
+            },
+            {
+                vaule: '2',
+                name: '1. Crear tarea'
+            },
+            {
+                vaule: '3',
+                name: '3. Listar tareas pendientes'
+            },
+            {
+                vaule: '4',
+                name: '4. Completar tarea(s)'
+            },
+            {
+                vaule: '5',
+                name: '5. Limpiar tareas completadas'
+            },
+            {
+                vaule: '6',
+                name: '6. Borrar tarea'
+            },
+            {
+                vaule: '0',
+                name: '0. Salir'
+            },
+        ]
     }
 ];
+
 const inquirerMenu=async()=>{
-    console.clear();
+    //console.clear();
     console.log('====================='.green);
     console.log('SELECCIONE UNA OPCIóN'.green);
     console.log('=====================\n'.green);
 
-    const opt=await inquirer.prompt(preguntas);
+    const {opcion}=await inquirer.prompt(preguntas);
 
-    return opt;
+    return opcion;
 }
 
 
